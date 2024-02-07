5c89eed6acdc138c371fc67e932fdeabb1322727
DongKyu Lee <dldlehdrbrb@gmail.com>
2024-02-07 16:36:58 +0900

    레디스 비밀번호 설정 재수정

--- src/main/kotlin/com/sparta/restaurant_search/redis/RedisConfig.kt
+++ src/main/kotlin/com/sparta/restaurant_search/redis/RedisConfig.kt
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisPassword
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.serializer.StringRedisSerializer
@@ -14,11 +16,18 @@ class RedisConfig {
     @Value("\${spring.data.redis.host}")
     private val host: String = ""
     @Value("\${spring.data.redis.port}")
-    private val port: Int = 0
+    private val port1: Int = 0
+    @Value("\${spring.data.redis.password}")
+    private val password1: String = ""
 
     @Bean
     fun redisConnectionFactory(): RedisConnectionFactory {
-        return LettuceConnectionFactory(host, port)
+        val redisStandaloneConfiguration = RedisStandaloneConfiguration().apply {
+            this.hostName = host
+            this.port = port1
+            this.password = RedisPassword.of(password1)
+        }
+        return LettuceConnectionFactory(redisStandaloneConfiguration)
     }
 
     @Bean
