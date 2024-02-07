6c9a1e4795869743d7550d34b01d53ee38a0930c
DongKyu Lee <dldlehdrbrb@gmail.com>
2024-02-07 16:22:18 +0900

    레디스 비밀번호 설정 수정

--- src/main/kotlin/com/sparta/restaurant_search/redis/RedisConfig.kt
+++ src/main/kotlin/com/sparta/restaurant_search/redis/RedisConfig.kt
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
-import org.springframework.data.redis.connection.RedisPassword
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.serializer.StringRedisSerializer
@@ -17,25 +15,16 @@ class RedisConfig {
     private val host: String = ""
     @Value("\${spring.data.redis.port}")
     private val port: Int = 0
-    @Value("\${spring.data.redis.password}")
-    private val password: String = ""
 
     @Bean
     fun redisConnectionFactory(): RedisConnectionFactory {
-        val redisStandaloneConfiguration = RedisStandaloneConfiguration().apply {
-            hostName = host
-            port = port
-            password = password
-        }
-
-        val lettuceConnectionFactory = LettuceConnectionFactory(redisStandaloneConfiguration)
-        return lettuceConnectionFactory
+        return LettuceConnectionFactory(host, port)
     }
 
     @Bean
     fun redisTemplate(redisConnectionFactory: RedisConnectionFactory): RedisTemplate<String, String> {
         val redisTemplate = RedisTemplate<String, String>()
-        redisTemplate.connectionFactory = redisConnectionFactory()
+        redisTemplate.connectionFactory = redisConnectionFactory
         val serializer = StringRedisSerializer(Charset.forName("UTF-8"))
         redisTemplate.keySerializer = serializer
         redisTemplate.valueSerializer = serializer
