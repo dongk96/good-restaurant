0ad24647e5246ca78f22f3f3e21aeaa0abf5bcf1
DongKyu Lee <dldlehdrbrb@gmail.com>
2024-02-20 13:33:27 +0900

    ip 기반 위치 찾기 naver Geolocation api 도입 코드 수정

--- src/main/kotlin/com/sparta/restaurant_search/naver/NaverGeolocation.kt
+++ src/main/kotlin/com/sparta/restaurant_search/naver/NaverGeolocation.kt
@@ -3,6 +3,7 @@ package com.sparta.restaurant_search.naver
 import com.sparta.restaurant_search.web.reponse.SearchNaverGeoResponse
 import com.sparta.restaurant_search.web.reponse.SearchNaverResponse
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.configurationprocessor.json.JSONObject
 import org.springframework.core.ParameterizedTypeReference
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
@@ -13,6 +14,9 @@ import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponentsBuilder
 import java.sql.Timestamp
 import java.time.LocalDateTime
+import java.util.*
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
 
 @Component
 class NaverGeolocation(
@@ -22,16 +26,24 @@ class NaverGeolocation(
     @Value("\${naver.geolocation.secret}")
     private val naverGeoSecret: String,
 
-    @Value("\${naver.geolocation.url}")
-    private val naverGeoUrl: String
 ) {
+    val accessKey: String = System.getenv(naverGeoId) // 발급받은 API Key 입력
+    val secretKey: String = System.getenv(naverGeoSecret) // 발급받은 Secret Key 입력
+    val hostName = "https://geolocation.apigw.ntruss.com"
+    val requestUrl = "/geolocation/v2/geoLocation"
+
     fun search(ip: String): SearchNaverGeoResponse {
-        val uri = UriComponentsBuilder
-            .fromUriString(naverGeoUrl)
-            .queryParam(ip)
-            .build()
-            .encode()
-            .toUri()
+
+        val sortedSet = TreeMap<String, String>()
+        sortedSet["ip"] = ip
+        sortedSet["ext"] = "t"
+        sortedSet["responseFormatType"] = "json"
+
+        val timeStamp = System.currentTimeMillis().toString()
+        val queryString = sortedSet.map { "${it.key}=${it.value}" }.joinToString("&")
+        val baseString = "$requestUrl?$queryString"
+        val signature = makeSignature(secretKey, "GET", baseString, timeStamp, accessKey)
+
 
         val headers = HttpHeaders()
         headers.set("x-ncp-apigw-timestamp", System.currentTimeMillis().toString())
@@ -39,18 +51,25 @@ class NaverGeolocation(
         headers.set("X-Naver-Client-Secret", naverGeoSecret)
         headers.contentType = MediaType.APPLICATION_JSON
 
-        val httpEntity = HttpEntity<Any>(headers)
-        val responseType = object : ParameterizedTypeReference<SearchNaverGeoResponse>() {}
+        val restTemplate = RestTemplate()
+        val response = restTemplate.getForObject("$hostName$baseString", String::class.java)
+        if (response != null) {
+            val jsonObject = JSONObject(response)
+            val lat = jsonObject.getDouble("lat")
+            val long = jsonObject.getDouble("long")
+            return SearchNaverGeoResponse(lat, long)
+        } else {
+            throw IllegalStateException("Failed to retrieve search results.")
+        }
+    }
 
-        val responseEntity = RestTemplate().exchange(
-            uri,
-            HttpMethod.GET,
-            httpEntity,
-            responseType
-        )
+    fun makeSignature(secretKey: String, method: String, baseString: String, timestamp: String, accessKey: String): String {
+        val hmacSha256 = Mac.getInstance("HmacSHA256")
+        val secretKeySpec = SecretKeySpec(secretKey.toByteArray(), "HmacSHA256")
+        hmacSha256.init(secretKeySpec)
 
-        return responseEntity.body ?: throw IllegalStateException("Failed to retrieve search results.")
-
-
+        val message = "$method\n$baseString\n$timestamp\n$accessKey"
+        val hash = hmacSha256.doFinal(message.toByteArray())
+        return Base64.getEncoder().encodeToString(hash)
     }
 }
